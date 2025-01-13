8c92b5cfff377944b73abb7dda6f5a18f271f41b
[anudeep avula] <[anudeepavula009@gmail.com]>
2025-01-13 10:40:27 +0530

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <p className='bg-yellow-500 flex justify-center'>react+typescript+tailwind</p>
+    </>
+  )
+}
+
+export default App
