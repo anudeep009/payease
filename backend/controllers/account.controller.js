dca05fb0b7b9e18fcdc624799c20563f837e0067
[anudeep avula] <[anudeepavula009@gmail.com]>
2025-01-13 19:07:37 +0530

    feat(transfer):add transfer feat

--- backend/controllers/account.controller.js
+++ backend/controllers/account.controller.js
@@ -34,4 +34,55 @@ const getBalance = async (req, res) => {
   }
 };
 
-export { getBalance };
+const transfer = async (req, res) => {
+  const session = await mongoose.startSession();
+  try {
+    const { toUserId, amount } = req.body;
+
+    if (amount <= 0) {
+      return res.status(400).json({ message: "Invalid transfer amount" });
+    }
+
+    session.startTransaction();
+
+    const account = await Account.findOne({ userid: req.user.userId }).session(
+      session
+    );
+    if (!account || account.balance < amount) {
+      await session.abortTransaction();
+      return res.status(400).json({ message: "Insufficient balance" });
+    }
+
+    const toAccount = await Account.findOne({
+      userid: new mongoose.Types.ObjectId(toUserId),
+    }).session(session);
+    if (!toAccount) {
+      await session.abortTransaction();
+      return res.status(400).json({ message: "Invalid To account" });
+    }
+
+    await Account.updateOne(
+      { userid: req.user.userId },
+      { $inc: { balance: -amount } }
+    ).session(session);
+
+    await Account.updateOne(
+      { userid: toUserId },
+      { $inc: { balance: amount } }
+    ).session(session);
+
+    await session.commitTransaction();
+    return res.status(200).json({ message: "Transfer Successful" });
+  } catch (error) {
+    await session.abortTransaction();
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Error during Transfer", error: error.message });
+  } finally {
+    session.endSession();
+  }
+};
+
+
+export { getBalance, transfer };
--- backend/routes/account.routes.js
+++ backend/routes/account.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import verifyJWT from "../middlewares/verifyJWT.js";
-import { getBalance } from "../controllers/account.controller.js";
+import { getBalance, transfer } from "../controllers/account.controller.js";
 
 const router = Router();
 
+//route for to fetch balance
 router.get("/balance/:userid",verifyJWT, getBalance);
+
+//route for transfer amount
+router.post("/transfer",verifyJWT, transfer);
 
 export default router;
